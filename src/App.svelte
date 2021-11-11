431045fbe620936eb2c3f9eef7d3446d4fb513e8
Jasper Tung <jtunggsp@gmail.com>
2021-11-11 14:01:46 -0800

    added background and glass card

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,58 @@
 <script>
-  let username = ""
-  let password = ""
+  let username = "";
+  let password = "";
 
   function logIn() {
-    return username
-    return password
+    return {
+      username,
+      password,
+    };
   }
 </script>
 
 <main data-theme="corporate">
-  <div class="md:container md:max-auto md:space-y-6 bg-white">
-    <div class="form-control">
-      <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label class="label">
-        <span class="label-text">Username</span>
-      </label> 
-      <input type="text" placeholder="username" class="input input-bordered lg:w-60" bind:value={username}>
-    </div>
+  <div class="hero min-h-screen" style="background-image: url(src/assets/presentation-intro-2.png);">
+    <div class="hero-overlay bg-opacity-0"></div> 
+      <div class="card glass lg:card-side text-neutral-content">
+        <div class="max-w-md card-body items-center">
+          <h2 class="card-title">Log In</h2>
+          <div class="form-control">
+            <label for="username" class="label">
+              <span class="label-text">Username</span>
+            </label>
+            <input
+              id="username"
+              type="text"
+              placeholder="username"
+              class="input input-bordered lg:w-60"
+              bind:value={username}
+            />
+          </div>
 
-    <div class="form-control">
-      <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label class="label">
-        <span class="label-text">Password</span>
-      </label> 
-      <input type="text" placeholder="password" class="input input-bordered lg:w-60" bind:value={password}>
+          <div class="form-control">
+            <label for="password" class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="password"
+              class="input input-bordered lg:w-60"
+              bind:value={password}
+            />
+          </div>
+          <div class="card-actions">
+            <button class="btn btn-primary lg:w-36" on:click={logIn}>Log In</button>
+          </div>
+          <!-- <p>Logging into {username}'s account</p> -->
+        </div>
+      </div>
+      <!-- if block -->
     </div>
-      
-    <button class="btn btn-primary lg:w-60" on:click={logIn}>Log In</button>
-    <p>Logging into {username}'s account</p>
-  </div>
 </main>
 
 <style>
-
+  main {
+    height: 100vh;
+  }
 </style>
